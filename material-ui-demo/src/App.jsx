53bd1b90bc2af3b5ae36bcee9cab836e6e83b908
Sanjeev Pandey <sanjeevpandey467@gmail.com>
2024-07-14 12:23:50 +0530

    Raect Basics

--- material-ui-demo/src/App.jsx
+++ material-ui-demo/src/App.jsx
@@ -1,14 +1,20 @@
 import "./App.css";
 import Button from "@mui/material/Button";
+import RatingDemo from "./RatingDemo";
 
 function App() {
   return (
+    // <>
+    //   <Button variant="contained" onClick={() => alert("hii")}>
+    //     contained
+    //   </Button>
+    //   <Button variant="text">Contained</Button>
+    //   <Button variant="outlined">Contained</Button>
+
+    // </>
+
     <>
-      <Button variant="contained" onClick={() => alert("hii")}>
-        contained
-      </Button>
-      <Button variant="text">Contained</Button>
-      <Button variant="outlined">Contained</Button>
+      <RatingDemo />
     </>
   );
 }
